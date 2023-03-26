05160620f5426adc9613515ed1c506c0d182dce5
Anastasiya <kuznecovaas94@mail.ru>
2023-03-27 01:58:43 +0300

    seminar1

---
+++ Seminar 1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Enter numberA : ");
+Console.WriteLine("Enter numberB : ");
+
+string strNumA = Console.ReadLine();
+string strNumB = Console.ReadLine();
+
+int numberA = Convert.ToInt32(strNumA); 
+int numberB = Convert.ToInt32(strNumB); 
+
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
+
+
---
+++ Seminar 1.3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели
+// по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+Console.WriteLine("Vvedite chislo day");
+int numberDayOfWeek = Convert.ToInt32(Console.ReadLine());
+    if (numberDayOfWeek == 1)
+
+    {
+        Console.WriteLine("понедельник");
+    }
+        else if (numberDayOfWeek == 2)
+        {
+           Console.WriteLine("вторник"); 
+        }
+       
+        else if (numberDayOfWeek == 3)
+        {
+            Console.WriteLine("среда");
+        }
+        else if (numberDayOfWeek == 4)
+        {
+            Console.WriteLine("четверг");
+        }
+        else if (numberDayOfWeek == 5)
+        {
+            Console.WriteLine("пятница");
+        }
+        else if (numberDayOfWeek == 6)
+        {
+            Console.WriteLine("суббота");
+        }
+        else if (numberDayOfWeek == 7)
+        {
+            Console.WriteLine("воскресенье");
+        }
+    else
+    {
+        Console.WriteLine("такого дня не существует");
+    }   
+        
+        
+        
+        
+        
+       
+        
